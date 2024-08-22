313f299b68cb39776fa2e1cbd0ff1fda73cedbd8
Robby <robby@henderson-woods.com>
2024-08-21 23:27:04 -0500

    worked on api and routes

--- 02-Challenge/Develop/server/src/routes/api/weatherRoutes.ts
+++ 02-Challenge/Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,40 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
 
-// TODO: POST Request with city name to retrieve weather data
+import WeatherService from '../../service/weatherService.js';
+
+
+// Dne: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+
+  // Done: GET weather data from city name
+  getWeather(req.body.city)
+  // Done: save city to search history
+  saveCity(req.body.city)
 });
 
-// TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+// DOne: GET search history
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getCities();
+    res.status(200).json(history);
+  } catch (error) {
+    res.status(500).json({ error: 'Could not get search history', });
+  }
+});
+
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try {
+    await HistoryService.removeCity(req.params.id);
+    res.status(200).json({ message: 'City removed from search history', });
+  } catch (error) {
+    res.status(500).json({ error: 'Could not remove city from search history', });
+  }
+
+});
 
 export default router;
--- 02-Challenge/Develop/server/src/service/historyService.ts
+++ 02-Challenge/Develop/server/src/service/historyService.ts
@@ -1,7 +1,33 @@
 // TODO: Define a City class with name and id properties
 
+import { promises as fs } from 'fs';
+
+class City {
+  constructor(public id: string, public name: string) { }
+}
+
+
 // TODO: Complete the HistoryService class
 class HistoryService {
+  getCities() {
+    throw new Error('Method not implemented.');
+  }
+  private filePath = 'searchHistory.json';
+
+  // Read method to read from the searchHistory.json file
+  private async read(): Promise<City[]> {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      return JSON.parse(data) as City[];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        // If file does not exist, return an empty array
+        return [];
+      }
+      throw error;
+    }
+  }
+
   // TODO: Define a read method that reads from the searchHistory.json file
   // private async read() {}
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
@@ -14,4 +40,11 @@ class HistoryService {
   // async removeCity(id: string) {}
 }
 
+    // Write method to write to the searchHistory.json file
+    private async write(cities: City[]): Promise < void> {
+  await fs.writeFile(this.filePath, JSON.stringify(cities));
+}
+
 export default new HistoryService();
+
+
